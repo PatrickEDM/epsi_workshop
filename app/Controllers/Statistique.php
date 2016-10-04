1de922cbef4589bb2786dd10f5257bddee98207b
jordandevolder <contact@jordandevolder.fr>
2016-10-04 16:00:51 +0200

    Ajout du controlleur statistique

---
+++ app/Controllers/Statistique.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jordan
+ * Date: 04/10/16
+ * Time: 15:50
+ */
+
+namespace Controllers;
+
+
+use Core\Controller;
+
+class Statistique extends Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+
+    }
+
+
+
+
+}
