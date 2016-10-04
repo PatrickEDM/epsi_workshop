add0917a83f9360fab93350d831e7368286717d6
jordandevolder <contact@jordandevolder.fr>
2016-10-04 14:44:48 +0200

    AJout/Suppression/Memorisation des joueurs dans le controlleur superviseur

--- app/Controllers/Superviseur.php
+++ app/Controllers/Superviseur.php
@@ -12,18 +12,20 @@ namespace Controllers;
 use Core\Controller;
 use Helpers\DB\EntityManager;
 use Helpers\Session;
+use Models\Queries\JoueurSQL;
 use Models\Tables\Joueur;
 
 class Superviseur extends Controller
 {
 
     private $entityManager;
+    private $joueurSQL;
 
     public function __construct()
     {
         parent::__construct();
         $this->entityManager = EntityManager::getInstance();
-
+        $this->joueurSQL = new JoueurSQL();
     }
 
 
@@ -32,12 +34,42 @@ class Superviseur extends Controller
         // Récuperation des variables via un formulaire pour ajouter une personne sous sa tutelle
         $prenom =  $_POST['prenom'];
         $nom = $_POST['nom'];
+        $condition = " nom = ".$nom." and prenom = ".$prenom;
+        $joueur = $this->joueurSQL->prepareFindWithCondition($condition)->execute();
+        if($joueur)
+        {
+            echo "Ce joueur existe déjà";
+        }
+        else
+        {
+            $idSuperviseur = Session::get('id');
+            // Ajout en base du joueur
+            $joueur = new Joueur($prenom,$nom,$idSuperviseur);
+            $this->entityManager->save($joueur);
+        }
+    }
+
+    public function supprimerJoueur()
+    {
+        $prenom = "foo";
+        $nom = "bar";
         $idSuperviseur = Session::get('id');
+        $idJoueur = -1;
 
-        // Ajout en base du joueur
-        $joueur = new Joueur($prenom,$nom,$idSuperviseur);
-        $this->entityManager->save($joueur);
+        $joueur = new Joueur($prenom,$nom,$idSuperviseur,$idJoueur);
+        $this->entityManager->delete($joueur);
 
+
+    }
+
+    public function connexionJoueur($joueur)
+    {
+        // /!\ Attention, aucun rapport avec la connexion, on garde juste en session les données relatives au Joueur sous la tutelle du
+        // superviseur connecté
+
+        Session::set('idJoueur',$joueur->id);
+        Session::set('nomJoueur',$joueur->nom);
+        Session::set('prenomJoueur',$joueur->prenom);
     }
 
 }
