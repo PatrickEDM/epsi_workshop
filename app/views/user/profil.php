339149edcd9946dda35c1cbb81183ff5e098d384
jordandevolder <contact@jordandevolder.fr>
2016-10-05 11:26:46 +0200

    AJout foreach dans profil.php, servira a créer les charts

--- app/views/user/profil.php
+++ app/views/user/profil.php
@@ -9,46 +9,55 @@
 ?>
 <div class="profil">
     <h3><?= $data['joueur']->prenom." ".$data['joueur']->nom; ?></h3>
-<div class="row">
-    <div class="col-md-6 col-sm-8">
-        <p>Jeux 1</p>
-        <div class="ct-chart ct-perfect-fourth"></div>
-        Créer fonction récupère nom jeux <br/>
-        Créer fonction foreach game : Chartiste.Line('.ct-chart[i]',
-        <script>
-		    new Chartist.Line('.ct-chart', {
-  			    labels: ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet'],
-  			    series: [
-    			    [12, 9, 7, 8, 5],
-  			    ]
-		    }, {
-  				fullWidth: false,
-  				height: 300,
-  				width: 600,
-  				chartPadding: {
-    				right: 60
-  				}
-			});
-        </script>
-    </div>
-    <div class="col-md-3 col-sm-8">
-        <p>Jeux 2</p>
-        <div class="ct-chart1 ct-perfect-fourth"></div>
-        <script>
-		    new Chartist.Line('.ct-chart1', {
-  			    labels: ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet'],
-  			    series: [
-    			    [1, 9, 7, 8, 5],
-  			    ]
-		    }, {
-  				fullWidth: false,
-  				height: 300,
-  				width: 600,
-  				chartPadding: {
-    				right: 60
-  				}
-			});
-        </script>
-    </div>
+    <div class="row">
+        <div class="col-md-6 col-sm-8">
+            <p>Jeux 1</p>
+            <div class="ct-chart ct-perfect-fourth"></div>
+            <?php
+                // affiche toutes les stats du joueur connecté
+                foreach ($data['stat'] as $stat)
+                {
+                    echo $stat->date."<br>";
+                    echo $stat->score."<br>";
+                    echo $stat->nomJeu."<br>";
+                    echo "<br>";
+                }
+
+            ?>
+            <script>
+                new Chartist.Line('.ct-chart', {
+                    labels: ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet'],
+                    series: [
+                        [12, 9, 7, 8, 5],
+                    ]
+                }, {
+                    fullWidth: false,
+                    height: 300,
+                    width: 600,
+                    chartPadding: {
+                        right: 60
+                    }
+                });
+            </script>
+        </div>
+        <div class="col-md-3 col-sm-8">
+            <p>Jeux 2</p>
+            <div class="ct-chart1 ct-perfect-fourth"></div>
+            <script>
+                new Chartist.Line('.ct-chart1', {
+                    labels: ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet'],
+                    series: [
+                        [1, 9, 7, 8, 5],
+                    ]
+                }, {
+                    fullWidth: false,
+                    height: 300,
+                    width: 600,
+                    chartPadding: {
+                        right: 60
+                    }
+                });
+            </script>
+        </div>
     </div>
 </div>
