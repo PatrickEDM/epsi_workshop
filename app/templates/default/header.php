a58fa3ed55a8682382c810cd9b268a8fd6804630
Sullivan <villellasullivan@gmail.com>
2016-10-05 18:15:49 +0200

    DropDown

--- app/templates/default/header.php
+++ app/templates/default/header.php
@@ -64,7 +64,18 @@ $hooks = Hooks::get();
                 ?>
                 <li class="welcome"><a href="#"><?= "Bienvenue ".Session::get('prenomJoueur'); ?></a></li>
                 <li><a href='<?= DIR?>profil/<?= Session::get('idJoueur'); ?>'>Profil</a></li>
-                <li><a href='<?= DIR?>jeux/'>Jeux</a></li>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Jeux<span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="#">Memory</a></li>
+                        <li><a href="#">Wordcases</a></li>
+                        <li><a href="#">Something else here</a></li>
+                        <li role="separator" class="divider"></li>
+                        <li><a href="#">Separated link</a></li>
+                        <li role="separator" class="divider"></li>
+                        <li><a href="#">One more separated link</a></li>
+                    </ul>
+                </li>
                 <li><a href='<?= DIR?>desactiver_joueur'>Deconnexion</a></li>
                 <?php
                 else:
