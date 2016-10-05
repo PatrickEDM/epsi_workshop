da803fd58819ade6cba87d33011bffed03e31bbf
Sullivan <villellasullivan@gmail.com>
2016-10-05 09:39:17 +0200

    Overflow fix , responsive table

--- app/views/manage_user/users.php
+++ app/views/manage_user/users.php
@@ -13,30 +13,34 @@
     <input type="text" name="prenom" placeholder="Prénom">
     <button type="submit" id="joueur-button">Ajouter un utilisateur</button>
 </form>
+<div class="row">
+    <div class="col-md-12 col-sm-4">
+        <table class="table table-hover">
+            <thead>
+            <tr>
+                <th>Nom</th>
+                <th>Prénom</th>
+                <th>Profil</th>
+                <th>Activer</th>
+                <th>Supprimer</th>
+            </tr>
+            </thead>
+            <tbody>
+            <?php
 
-<table class="table table-hover">
-    <thead>
-    <tr>
-        <th>Nom</th>
-        <th>Prénom</th>
-        <th>Profil</th>
-        <th>Activer</th>
-        <th>Supprimer</th>
-    </tr>
-    </thead>
-    <tbody>
-    <?php
-    foreach($data['joueurs'] as $j):
-        ?>
-        <tr>
-            <td><?= $j->nom; ?></td>
-            <td><?= $j->prenom; ?></td>
-            <td><a href="<?= DIR ?>profil/<?= $j->getId(); ?>" class="btn btn-success" role="button">Profil</a></td>
-            <td><a href="<?= DIR ?>activer_joueur/<?= $j->getId(); ?>" class="btn btn-primary" role="button">Activer</a></td>
-            <td><a href="<?= DIR ?>supprimer_joueur/<?= $j->getId(); ?>" class="btn btn-danger" role="button">Supprimer</a></td>
-        </tr>
-        <?php
-    endforeach;
-    ?>
-    </tbody>
-</table>
+foreach($data['joueurs'] as $j):
+?>
+            <tr>
+                <td><?php echo $j->nom; ?></td>
+                <td><?php echo $j->prenom; ?></td>
+                <td><a href="<?php echo DIR ?>profil/<?php echo $j->getId(); ?>" class="btn btn-success" role="button">Profil</a></td>
+                <td><a href="<?php echo DIR ?>activer_joueur/<?php echo $j->getId(); ?>" class="btn btn-primary" role="button">Activer</a></td>
+                <td><a href="<?php echo DIR ?>supprimer_joueur/<?php echo $j->getId(); ?>" class="btn btn-danger" role="button">Supprimer</a></td>
+            </tr>
+            <?php
+endforeach;
+?>
+            </tbody>
+        </table>
+    </div>
+</div>
